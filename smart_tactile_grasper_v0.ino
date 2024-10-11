3b6b0ac3e32f828bf722719429f702d2532544dd
Marielle Hicban <54655783+marbhic@users.noreply.github.com>
2024-10-11 13:15:41 -0500

    initial code version

---
+++ smart_tactile_grasper_v0.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo myservo;
+const byte strain_gauge_pin = A0; 
+
+void setup()
+{
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+
+  // linear actuator is connected to pin 9
+  myservo.attach(9);
+  
+  // set servo to mid-point
+//  myservo.writeMicroseconds(1000);  
+}
+
+void loop() {
+  int sensorValue = analogRead(strain_gauge_pin);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(100);        // delay in between reads for stability
+  }
